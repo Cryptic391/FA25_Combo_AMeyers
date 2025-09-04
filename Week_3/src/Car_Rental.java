867655c71f7d7c6f8bbb8287de9e411cd99345ca
Meyers <S02735264@ccc.ccofc.edu>
2025-09-04 10:05:47 -0600

    Full Car Rental

--- Week_3/src/Car_Rental.java
+++ Week_3/src/Car_Rental.java
@@ -7,48 +7,55 @@ public class Car_Rental {
 		Scanner input = new Scanner(System.in);
 		int days, miles, carCode, dayPrice;
 		double total, milePrice, dayTotal;
-		char anotherCustomer = 'y';
+		String another = "y";
 		dayTotal = 0;
 		total = 0;
 		
-		if (anotherCustomer == 'y')
+		while (another.equals("y"))
 			
 		{
 			
-		System.out.println("Car Code?");
-		carCode = input.nextInt();
-	
-		if(carCode == 1)
-		{
-			dayPrice = 75;
-			milePrice = 1.5;
-		}	
+			System.out.println("Car Code?");
+			carCode = input.nextInt();
 		
-		else if (carCode == 2)
-		{
-			dayPrice = 45;
-			milePrice = .85;
+			if(carCode == 1)
+			{
+				dayPrice = 75;
+				milePrice = 1.5;
+			}	
 			
+			else if (carCode == 2)
+			{
+				dayPrice = 45;
+				milePrice = .85;
+				
+			}
+			
+			else
+			{
+				dayPrice = 35;
+				milePrice = .45;
+			}
+			
+			System.out.println("How many Days?");
+			days = input.nextInt();
+			
+			System.out.println("How mant miles?");
+			miles = input.nextInt();
+			
+			total = days * dayPrice + miles * milePrice;
+			System.out.printf("Your total is $%.2f.\n", total);
+			
+			System.out.println("Is there another customer?\n" + "y or n?");
+			another = input.next();
+			
+			dayTotal = dayTotal + total;
+		
 		}
-		
-		else
-		{
-			dayPrice = 35;
-			milePrice = .45;
-		}
-		
-		System.out.println("Days?");
-		days = input.nextInt();
-		
-		System.out.println("Miles?");
-		miles = input.nextInt();
-		
-		total = days * dayPrice + miles * milePrice;
-		System.out.printf("Your total is $%.2f", total);
-		
-		
-		
-		}
+			
+			
+		System.out.printf("Your total for the day is $%.2f", dayTotal);
+			
 
 	}
 
