b7cddc6608fbf3ece5096b445ae56c8594991026
Meyers <S02735264@ccc.ccofc.edu>
2025-09-17 09:56:05 -0600

    Scrabble 3  9/17/25

--- Week 5/src/Scrabble.java
+++ Week 5/src/Scrabble.java
@@ -32,29 +32,29 @@ public class Scrabble {
 		}
 		else
 		{
-			char[] tileRay = tiles.toCharArray();
+			char[] tileRay = tiles.toCharArray();		//creates an array with the characters in tiles
 			for (int i = 0; i < len; i++)		//loops a number of times equal to the length of word
 			{			
-//				char letter = word.charAt(i);		//sets the letter to the letter at position i		
+//				char letter = word.charAt(i);		//sets the letter to the letter at position i	(not needed)	
 			
-				for(int j = 0; j < len; j++)		//loops a number of times equal to the length of the word
+				for(int j = 0; j < tiles.length(); j++)		//loops a number of times equal to the length of the word
 				{
 					
-//					char tile = tiles.charAt(j);		//sets the tile to the letter at position j
+//					char tile = tiles.charAt(j);		//sets the tile to the letter at position j		(not needed)
 					
 					
 					if(word.charAt(i) == tileRay[j])		//if the character of tile j matches the character at word i
 					{
-						tileRay[j] = '*';
+						tileRay[j] = '*';		//sets character in tileRay at position j to *
 						count++;		//add 1 to count
-//						System.out.println(count);			//print count (test)
+//						System.out.println(count);			//print count (trouble shoot)
 						
 					}
 					
 					
 				}
 			}
-		if(count == word.length())		//if the count equals the word length
+		if(count == word.length()-1)		//if the count equals the word length
 		{
 			
 			return true;	//send true
@@ -62,7 +62,7 @@ public class Scrabble {
 		else
 		{
 			
-			return false;		//else send false
+			return false;		//otherwise send false
 		}
 		}
 		
